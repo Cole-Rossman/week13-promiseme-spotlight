44958776ce41ed5a1f2ebc0dc74ba9dbc71c8493
Cole Rossman <colerossman99@gmail.com>
2022-04-26 11:23:05 -0700

    class progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,26 @@
+import { getFuturamaQuotes, thenGetQuotes } from "./services/promise-me";
+
 export default function App() {
-  return <h1>Hello World</h1>;
+  const handleClick = (callback) => {
+    const result = callback();
+    console.log(result);
+  };
+
+  return (
+    <>
+    <button onClick= {async () => console.log(getFuturamaQuotes())}>
+      Async Get Quotes
+    </button>
+    <button onClick={async () => console.log(thenGetQuotes()
+    )}>
+      .then() Get Quotes
+    </button>
+    </>
+  );
 }
+// a higher order function takes a function as parameters and returns a function 
+// the return will show the result in the console
+// whenever async keyword is used, it will always return a promise
+// syntactic sugar are features of a programming language or api easier
+
+// with the console log example, non blocking (.then approach) completed the order of console log 1(start),3(end),2(middle) but async is blocking so it showed the console log in order 1(start),2(middle),3(end)
--- src/services/promise-me.js
+++ src/services/promise-me.js
@@ -1,19 +1,44 @@
 /**
  * TODO: Exercise 1: use `fetch` & async/await to get data from an API
  */
+    export async function getFuturamaQuotes() {
+        const resp = await fetch('futuramaapi.herokuapp.com/api/quotes');
+        const result = await resp.json();
+        const quote = result[0];
+        const totalResults = result.length;
 
+        return { quote: result[0], totalResults: result.length };
+    }
 /**
  * TODO: Exercise 2: use `fetch` & `.then` syntax to get the same data from the first exercise.
  */
+    export function thenGetQuotes() {
+        return fetch('futuramaapi.herokuapp.com/api/quotes').then((resp) => resp.json()
+        // above the resp is implicitly returned, no need to type return
+        .then((result) => ({ quote: result[0], totalResults: result.length }))
+        );
+    }
 
 /**
  * TODO: Exercise 3: use `fetch`, async/await, and a try/catch statement to get data from an API and handle errors
  */
+  export async function tryGetQuotes() {
+      try {
+        const resp = await fetch('futuramaapi.herokuapp.com/api/quotes');
+        const result = await resp.json();
+        const quote = result[0];
+        const totalResults = result.length;
+
+        return { quote: result[0], totalResults: result.length };
+      } catch (error) {
+          console.error('an error occurred')
+      }
+  }
 
 /**
  * TODO: Exercise 4: use `fetch`, `.then`, and `.catch` to get the same data from exercise 3 while handling errors
  */
-
+  
 /**
  * TODO: Exercise 5: use `fetch`, async/await, and a try/catch/finally statement to get data from an API, handle errors, then console.log('All done!') upon completion
  */
